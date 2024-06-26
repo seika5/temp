2a4775dc521a41d365a9a7df4e64555f7c195ec1
Ryan Mar <Phyre205@gmail.com>
2024-06-26 14:15:06 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Songs from "./Components/songs.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Songs></Songs>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/songs.js
@@ -0,0 +1,66 @@
+import '../App.css';
+import ReactPlayer from 'react-player';
+
+const song = [
+	"https://www.youtube.com/watch?v=F1yq_J7c6bM",
+	"https://www.youtube.com/watch?v=OjNpRbNdR7E",
+	"https://www.youtube.com/watch?v=ilhQlY7kP5Y",
+	"https://www.youtube.com/watch?v=dJs04lHumSA"];
+
+const reload = () => {
+	window.location.reload();
+}
+
+const Songs = () => {
+	return (
+		<div>
+			<button onClick={reload}>▶</button>
+
+			<ReactPlayer
+				className="react-player"
+				url={song[0]}
+				width="400px"
+				height="400px"
+				playing={true}
+				loop={true}
+				controls={true}
+				volume={0.1}
+			/>
+
+			<ReactPlayer
+				className="react-player"
+				url={song[1]}
+				width="400px"
+				height="400px"
+				playing={true}
+				loop={true}
+				controls={true}
+				volume={0.08}
+			/>
+
+			<ReactPlayer
+				className="react-player"
+				url={song[2]}
+				width="400px"
+				height="400px"
+				playing={true}
+				loop={true}
+				controls={true}
+				volume={0.08}
+			/>
+
+			<ReactPlayer
+				className="react-player"
+				url={song[3]}
+				width="400px"
+				height="400px"
+				playing={true}
+				loop={true}
+				controls={true}
+				volume={0.08}
+			/>
+		</div>
+	)
+}
+
+export default Songs
